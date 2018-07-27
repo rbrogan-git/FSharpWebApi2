bd0a12bb51cf7f2e24a0a16ccc852325781b9e05
Roger Brogan <roger.brogan@avanade.com>
2018-07-26 21:26:20 -0500

    Getting simple DTO passed back and forth.

---
+++ FSharpWebApi2/Controllers/OrdersController.fs
@@ -0,0 +1,35 @@
+﻿namespace FSharpWebApi2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open DTOs
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type OrdersController () =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member this.Get() =
+        let values = [|"value1"; "value2"|]
+        ActionResult<string[]>(values)
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:int) =
+        let value = "value"
+        ActionResult<string>(value)
+
+    [<HttpPost>]
+    member this.Post([<FromBody>] value : OrderFormDto) =
+        value
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>] value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
---
+++ FSharpWebApi2/DTOs.fs
@@ -0,0 +1,15 @@
+﻿module DTOs
+[<CLIMutable>]
+type CustomerInfoDto = {
+    FirstName : string
+    LastName : string
+    EmailAddress : string
+    }
+[<CLIMutable>]
+type OrderFormDto = {
+    OrderId : string
+    CustomerInfo : CustomerInfoDto
+    //ShippingAddress : AddressDto
+    //BillingAddress : AddressDto
+    //Lines : OrderFormLineDto list
+    }
